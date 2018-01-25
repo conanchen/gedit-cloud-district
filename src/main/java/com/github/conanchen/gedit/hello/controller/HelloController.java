fe77dfee9c98684d752e137c0b2e9cfbdbb5887f
conanchen <conan8chan@yahoo.com>
2018-01-25 17:00:29 +0800

    first commit

---
+++ src/main/java/com/github/conanchen/gedit/hello/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.github.conanchen.gedit.hello.controller;
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.text.DateFormat;
+import java.util.Date;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    private final static Gson gson = new Gson();
+    //
+//    @Autowired
+//    private WordRepository wordRepository;
+    @Value("${gedit.docker.enabled}")
+    Boolean insideDocker = false;
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return
+                String.format("hello@%s , HelloController Spring Boot insideDocker=%b",
+                        DateFormat.getInstance().format(new Date()), insideDocker);
+    }
+}
