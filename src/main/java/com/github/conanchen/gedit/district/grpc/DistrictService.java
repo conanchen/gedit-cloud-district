f257f2c215b782634f4164b33db28367ab5c84d5
conanchen <conan8chan@yahoo.com>
2018-01-25 22:39:10 +0800

    first commit

--- src/main/java/com/github/conanchen/gedit/district/grpc/DistrictService.java
+++ src/main/java/com/github/conanchen/gedit/district/grpc/DistrictService.java
@@ -1,67 +1,37 @@
 package com.github.conanchen.gedit.district.grpc;
 
 import com.github.conanchen.gedit.common.grpc.LogInterceptor;
-import com.github.conanchen.gedit.common.grpc.Status;
-import com.github.conanchen.gedit.hello.grpc.HelloGrpc;
-import com.github.conanchen.gedit.hello.grpc.HelloReply;
-import com.github.conanchen.gedit.hello.grpc.HelloRequest;
-import com.github.conanchen.gedit.hello.grpc.ListHelloRequest;
 import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
 
 @GRpcService(interceptors = {LogInterceptor.class})
-public class DistrictService extends HelloGrpc.HelloImplBase {
+public class DistrictService extends DistrictApiGrpc.DistrictApiImplBase {
     private static final Logger log = LoggerFactory.getLogger(DistrictService.class);
     private static final Gson gson = new Gson();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
-    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        final HelloReply.Builder replyBuilder = HelloReply.newBuilder()
-                .setStatus(Status.newBuilder()
-                        .setCode(io.grpc.Status.Code.OK.name())
-                        .setDetails("Hello很高兴回复你，你的hello很温暖。")
-                        .build())
-                .setUuid(UUID.randomUUID().toString())
-                .setMessage(String.format("Hello %s@%s ", request.getName(), dateFormat.format(System.currentTimeMillis())))
-                .setCreated(System.currentTimeMillis())
-                .setLastUpdated(System.currentTimeMillis());
-        HelloReply helloReply = replyBuilder.build();
-        responseObserver.onNext(helloReply);
-        log.info(String.format("HelloService.sayHello() %s:%s gson=%s", helloReply.getUuid(), helloReply.getMessage(), gson.toJson(helloReply)));
-        responseObserver.onCompleted();
+    public void upsert(UpsertDistrictRequest request, StreamObserver<DistrictResponse> responseObserver) {
+        super.upsert(request, responseObserver);
     }
 
     @Override
-    public void listOldHello(ListHelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        for (int i = 0; i < request.getSize(); i++) {
-            String uuid = UUID.randomUUID().toString();
-            final HelloReply.Builder replyBuilder = HelloReply.newBuilder()
-                    .setStatus(Status.newBuilder()
-                            .setCode(io.grpc.Status.Code.OK.name())
-                            .setDetails("Hello很高兴回复你，你的hello很温暖@" + DateFormat.getDateTimeInstance().format(new Date()))
-                            .build())
-                    .setUuid(uuid)
-                    .setMessage(String.format("Hello world%s@%s ", uuid, DateFormat.getDateTimeInstance().format(new Date())))
-                    .setCreated(System.currentTimeMillis())
-                    .setLastUpdated(System.currentTimeMillis());
-            HelloReply helloReply = replyBuilder.build();
-            responseObserver.onNext(helloReply);
-            try {
-                Thread.sleep(300);
-                log.info("sleep{} to send {}th hello", 300, i);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        responseObserver.onCompleted();
+    public void getTop(GetTopDistrictRequest request, StreamObserver<DistrictResponse> responseObserver) {
+        super.getTop(request, responseObserver);
+    }
+
+    @Override
+    public void listChild(ListChildDistrictRequest request, StreamObserver<DistrictResponse> responseObserver) {
+        super.listChild(request, responseObserver);
+    }
+
+    @Override
+    public void list(ListDistrictRequest request, StreamObserver<DistrictResponse> responseObserver) {
+        super.list(request, responseObserver);
     }
 }
